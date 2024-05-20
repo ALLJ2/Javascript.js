a4c9b90e3a987da2ad203ea0467969b7008f5a64
alexsandrolucianodejesus <alexsandro.ljesus@senacsp.edu.br>
2024-05-20 16:41:39 -0300

    Javascript

---
+++ js/script.js
@@ -0,0 +1,50 @@
+//interpolação interpolando um string
+const primeiroNome = "Alexsandro";
+console.log(`Olá, ${primeiroNome}`);
+
+//interpolação de numero
+const idade = 30;
+console.log(`sua idade é ${idade} anos `);
+
+//interpolação com expressão
+const resultado = 2+2;
+console.log(`o resultado do cálculo é ${resultado}`);
+
+//boolean - verdadeiro e falso
+const tarde = true;
+const noite = false;
+console.log(tarde);
+console.log(noite);
+
+const maioridade = 18;
+const consultaMaiorDeIdade = maioridade > 17;
+console.log(consultaMaiorDeIdade);
+
+//comparação
+const numero1 = 1;
+const numero2 = 5;
+console.log(numero1<numero2);
+console.log(numero1>numero2);
+const numero3 = 18;
+const numero4 = 18;
+console.log(numero3 >= numero4);
+console.log(numero4 <= numero3);
+
+//comparação de igualdade
+const a = 1;
+const b = 1;
+console.log(a===b);
+
+const c = 1;
+const d = "1";
+console.log(c==d);
+
+//comparação de diferenciação
+const e = 1;
+const f = "1";
+console.log(f != e);
+
+const g = 1;
+const h = 2;
+console.log(g !== h);
+
