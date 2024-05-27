dfba64f9a3a8f5aba805c78d9db55e77d94c2a19
alexsandrolucianodejesus <alexsandro.ljesus@senacsp.edu.br>
2024-05-27 16:01:29 -0300

    Exibi mensagens

---
+++ funcaoEobjeto/js/script.js
@@ -0,0 +1,38 @@
+// // Função de prompt
+// const nome = prompt("Qual é o seu nome?");
+// console.log(nome);
+
+// const idade = prompt("Qual a sua idade?");
+// console.log(idade);
+
+
+// // Função de alert
+// alert("Erro ao preencher o formulário");
+
+
+// Função Math; arredondando 3.6 para o inteiro mais próximo
+const round = Math.round(3.6);
+console.log(round);
+
+// retornar a raiz quadrada 
+const sqrt = Math.sqrt(16);
+console.log(sqrt);
+
+// Relevando 2 ao cubo
+const pow = Math.pow(2,3);
+console.log(pow);
+
+// exibindo uma mensagem de aviso no console
+console.warn("atention: esta é uma mensagem de aviso");
+
+// exibindo uma mensagem de erro no console
+console.error("Ocorreu um erro");
+
+// exibindo uma mensagem informativa no console
+console.info("Esta é uma mensagem informativa");
+
+// criando um grupo de mensagens
+console.group("Mensagem de teste");
+console.log("esta é uma mensagem de teste");
+console.log("esta é outra mensagem de teste");
+console.groupEnd();
